2452ac77150cf7985d90edecd6e4eac19d305e8c
Samim Hakimi <samimhakimi11@gmail.com>
2023-04-28 19:03:51 +0100

    Initialized App using Create-React-App and Clean Up

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Todo List App in React.js
+        </h1>
+
+    </div>
+  );
+}
+
+export default App;
