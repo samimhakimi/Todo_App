cf46193267a6fa6c145062394d66d3ad927eba84
Samim Hakimi <samimhakimi11@gmail.com>
2023-04-28 19:31:34 +0100

    Developed Input funtionalities and showing Todo List

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,46 @@
  import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addTodo = () => {
+      if(!value)
+       return alert("Pelase input Todo Task");
+     
+    const newItem = {
+      id: Math.random(),
+      value: value
+    }
+    setItems(oldItems => [...oldItems, newItem])
+    setValue("");
+
+  }
   return (
     <div className="App">
         <h1>
           Todo List App in React.js
         </h1>
 
+        <input
+         className='input'
+         placeholder='Type new Task to be added..'
+         on onChange={(e) => setValue(e.target.value)} 
+         value={value} /> 
+        <button onClick={addTodo} className='addButton'>Add Task</button>
+
+        <hr />
+        <ul>
+        {items.map(item => {
+          return(
+            <li>{item.value}</li>
+          )
+        })}
+        </ul>
     </div>
   );
 }
