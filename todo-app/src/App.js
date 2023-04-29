ea0eec179a2a8c145aa82f240f6a5e0cc874b04d
Samim Hakimi <samimhakimi11@gmail.com>
2023-04-29 10:51:57 +0100

    Developed Local Storage funtionalities

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,60 @@
  import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 
 function App() {
 
   const [value, setValue] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('SaveData')))
+
+
+ 
+
 
   const addTodo = () => {
       if(!value)
        return alert("Pelase input Todo Task");
      
     const newItem = {
-      id: Math.random(),
+      id: Math.floor(Math.random() * 100),
       value: value
     }
     setItems(oldItems => [...oldItems, newItem])
     setValue("");
 
+ 
+    if(localStorage.getItem('SaveData') == null)
+    {
+      localStorage.setItem('SaveData', '[]');
+    }
+  
+     var oldData = JSON.parse(localStorage.getItem('SaveData'));
+    oldData.push(newItem);
+ 
+
+    localStorage.setItem('SaveData', JSON.stringify(oldData))
+
   }
+
+  const deleteTask = (id) => {
+    const newArray = items.filter(item => item.id !== id)
+    setItems(newArray);
+ 
+    localStorage.setItem('SaveData', JSON.stringify(newArray))
+
+  }
+
+ 
+const bringTask = () => {
+  var oldData = JSON.parse(localStorage.getItem('SaveData'));
+  setItems(oldData);
+}
+ 
+
+
+
+
   return (
     <div className="App">
         <h1>
@@ -31,16 +66,22 @@ function App() {
          placeholder='Type new Task to be added..'
          on onChange={(e) => setValue(e.target.value)} 
          value={value} /> 
-        <button onClick={addTodo} className='addButton'>Add Task</button>
+        <button onClick={addTodo} className='addTask'>Add Task</button>
+        <button onClick={bringTask} className='addTask'>Bring Task</button>
 
         <hr />
         <ul>
-        {items.map(item => {
-          return(
-            <li>{item.value}</li>
-          )
+        {items && items.map(item => {
+          return(<>
+            <li key={item.id}>{item.value}</li>
+            <button className='deleteTask'
+            onClick={() => deleteTask(item.id)}
+            > Delete Task</button>
+             </>
+            )
         })}
         </ul>
+        {items  &&  "Total Task Remaining  " + items.length}
     </div>
   );
 }
